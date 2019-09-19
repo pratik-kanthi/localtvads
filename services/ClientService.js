ada4b268bb26ffd62de365c707bed9f6ae3d1ad8
Yogendra Yadav <yogendra@e9ine.com>
2019-09-19 16:22:29 +0100

    payment error fix

--- services/ClientService.js
+++ services/ClientService.js
@@ -1,7 +1,7 @@
 const Client = require.main.require('./models/Client').model;
 const ClientPaymentMethod = require.main.require('./models/ClientPaymentMethod').model;
 
-const {saveCustomer, saveNewCardToCustomer} = require.main.require('./services/PaymentService');
+const { saveCustomer, saveNewCardToCustomer } = require.main.require('./services/PaymentService');
 
 /*
  * Add card to a client
@@ -9,81 +9,84 @@ const {saveCustomer, saveNewCardToCustomer} = require.main.require('./services/P
  * @param {String} stripeToken - token of stripe
  */
 const addCard = (clientId, stripeToken) => {
-    return new Promise(async (resolve, reject) => {
-        if (!clientId) {
-            return reject({
-                code: 500,
-                error: {
-                    message: utilities.ErrorMessages.BAD_REQUEST
-                }
-            });
-        }
-        let query = {
-            Client: clientId
-        };
+	return new Promise(async (resolve, reject) => {
+		if (!clientId) {
+			return reject({
+				code: 500,
+				error: {
+					message: utilities.ErrorMessages.BAD_REQUEST
+				}
+			});
+		}
+		let query = {
+			Client: clientId
+		};
 
-        let isNew = false;
-        let newClientPaymentMethod;
-        let cardToken;
+		let isNew = false;
+		let newClientPaymentMethod;
+		let cardToken;
 
-        ClientPaymentMethod.findOne(query, async (err, clientPaymentMethod) => {
-            if (err) {
-                return reject({
-                    code: 500,
-                    error: err
-                });
-            } else if (!clientPaymentMethod) {
-                try {
-                    let client = await _getClient(clientId, {Email: 1});
-                    let csToken = await saveCustomer(stripeToken, client.Email);
-                    newClientPaymentMethod = new ClientPaymentMethod({
-                        StripeCusToken: csToken.id
-                    });
-                    cardToken = csToken.sources.data[0];
-                    isNew = true;
-                    newClientPaymentMethod.IsPreferred = true;
-                } catch (err) {
-                    return reject({
-                        code: err.code,
-                        error: err.error
-                    });
-                }
-            }
-            try {
-                cardToken = await saveNewCardToCustomer(isNew ? newClientPaymentMethod.StripeCusToken : clientPaymentMethod.StripeCusToken);
-                newClientPaymentMethod = new ClientPaymentMethod({
-                    StripeCusToken: clientPaymentMethod.StripeCusToken
-                });
-            } catch (ex) {
-                return reject({
-                    code: ex.code,
-                    error: ex.error
-                });
-            }
-            newClientPaymentMethod.Card = {
-                PaymentMethodType: 'CARD',
-                StripeCardToken: cardToken.id,
-                Vendor: cardToken.brand.toUpperCase(),
-                Name: cardToken.name,
-                ExpiryMonth: cardToken.exp_month,
-                ExpiryYear: cardToken.exp_year,
-                LastFour: cardToken.last4
-            };
-            newClientPaymentMethod.Client = clientId;
-            newClientPaymentMethod.save(err => {
-                if (err) {
-                    return reject({
-                        code: 500,
-                        error: err
-                    });
-                }
-                resolve({
-                    code: 200,
-                    data: newClientPaymentMethod
-                });
-            });
-        });
-    });
+		ClientPaymentMethod.findOne(query, async (err, clientPaymentMethod) => {
+			if (err) {
+				return reject({
+					code: 500,
+					error: err
+				});
+			} else if (!clientPaymentMethod) {
+				try {
+					let client = await _getClient(clientId, { Email: 1 });
+					let csToken = await saveCustomer(stripeToken, client.Email);
+					newClientPaymentMethod = new ClientPaymentMethod({
+						StripeCusToken: csToken.id
+					});
+					cardToken = csToken.sources.data[0];
+					isNew = true;
+					newClientPaymentMethod.IsPreferred = true;
+				} catch (err) {
+					return reject({
+						code: err.code,
+						error: err.error
+					});
+				}
+			}
+			try {
+				cardToken = await saveNewCardToCustomer(
+					stripeToken,
+					isNew ? newClientPaymentMethod.StripeCusToken : clientPaymentMethod.StripeCusToken
+				);
+				newClientPaymentMethod = new ClientPaymentMethod({
+					StripeCusToken: clientPaymentMethod.StripeCusToken
+				});
+			} catch (ex) {
+				return reject({
+					code: ex.code,
+					error: ex.error
+				});
+			}
+			newClientPaymentMethod.Card = {
+				PaymentMethodType: 'CARD',
+				StripeCardToken: cardToken.id,
+				Vendor: cardToken.brand.toUpperCase(),
+				Name: cardToken.name,
+				ExpiryMonth: cardToken.exp_month,
+				ExpiryYear: cardToken.exp_year,
+				LastFour: cardToken.last4
+			};
+			newClientPaymentMethod.Client = clientId;
+			newClientPaymentMethod.save((err) => {
+				if (err) {
+					return reject({
+						code: 500,
+						error: err
+					});
+				}
+				resolve({
+					code: 200,
+					data: newClientPaymentMethod
+				});
+			});
+		});
+	});
 };
 
 /*
@@ -91,32 +94,32 @@ const addCard = (clientId, stripeToken) => {
  * @param {String} clientId - _id of the client
  */
 const getSavedCards = (clientId) => {
-    return new Promise(async (resolve, reject) => {
-        let query = {
-            Client: clientId
-        };
-        let project = {
-            _id: 1,
-            IsPreferred: 1,
-            "Card.Vendor": 1,
-            "Card.Name": 1,
-            "Card.ExpiryMonth": 1,
-            "Card.ExpiryYear": 1,
-            "Card.LastFour": 1
-        };
-        ClientPaymentMethod.find(query, project).sort({IsPreferred: -1}).exec((err, cards) => {
-            if (err) {
-                return reject({
-                    code: 500,
-                    error: err
-                })
-            }
-            resolve({
-                code: 200,
-                data: cards
-            })
-        });
-    });
+	return new Promise(async (resolve, reject) => {
+		let query = {
+			Client: clientId
+		};
+		let project = {
+			_id: 1,
+			IsPreferred: 1,
+			'Card.Vendor': 1,
+			'Card.Name': 1,
+			'Card.ExpiryMonth': 1,
+			'Card.ExpiryYear': 1,
+			'Card.LastFour': 1
+		};
+		ClientPaymentMethod.find(query, project).sort({ IsPreferred: -1 }).exec((err, cards) => {
+			if (err) {
+				return reject({
+					code: 500,
+					error: err
+				});
+			}
+			resolve({
+				code: 200,
+				data: cards
+			});
+		});
+	});
 };
 
 /*
@@ -125,49 +128,49 @@ const getSavedCards = (clientId) => {
  * @param {String} cardId - stripe's card token
  */
 const getPreferredCard = (client, cardId) => {
-    return new Promise(async (resolve, reject) => {
-        let query = {
-            Client: client,
-            _id: cardId,
-            IsPreferred: true
-        };
-        try {
-            let card = await ClientPaymentMethod.findOne(query, {CardToken: 1, CustomerToken: 1});
-            if (!card) {
-                return reject({
-                    code: 404,
-                    error: {
-                        message: 'Card' + utilities.ErrorMessages.NOT_FOUND
-                    }
-                });
-            }
-            resolve(card);
-        } catch (err) {
-            return reject({
-                code: 500,
-                error: err
-            });
-        }
-    });
+	return new Promise(async (resolve, reject) => {
+		let query = {
+			Client: client,
+			_id: cardId,
+			IsPreferred: true
+		};
+		try {
+			let card = await ClientPaymentMethod.findOne(query, { CardToken: 1, CustomerToken: 1 });
+			if (!card) {
+				return reject({
+					code: 404,
+					error: {
+						message: 'Card' + utilities.ErrorMessages.NOT_FOUND
+					}
+				});
+			}
+			resolve(card);
+		} catch (err) {
+			return reject({
+				code: 500,
+				error: err
+			});
+		}
+	});
 };
 
 const _getClient = (client, projection) => {
-    return new Promise(async (resolve, reject) => {
-        Client.findOne({_id: client}, projection || {}, (err, client) => {
-            if (err) {
-                return reject({
-                    code: 500,
-                    error: err
-                });
-            } else {
-                resolve(client);
-            }
-        });
-    });
+	return new Promise(async (resolve, reject) => {
+		Client.findOne({ _id: client }, projection || {}, (err, client) => {
+			if (err) {
+				return reject({
+					code: 500,
+					error: err
+				});
+			} else {
+				resolve(client);
+			}
+		});
+	});
 };
 
 module.exports = {
-    addCard,
-    getPreferredCard,
-    getSavedCards
+	addCard,
+	getPreferredCard,
+	getSavedCards
 };
