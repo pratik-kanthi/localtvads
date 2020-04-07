9bef1551d5f59f4466b5ae666262015718829cdd
Pratik Kanthi <pratik.kanthi@e9ine.com>
2020-04-07 20:34:38 +0530

    fix: email template, subscriptions api

--- routes/SubscriberRoutes.js
+++ routes/SubscriberRoutes.js
@@ -1,10 +1,6 @@
-const {
-    getSubscribers,
-    unsubscribeUser
-} = require.main.require('./services/SubscriberService');
+const { getSubscribers, unsubscribeUser } = require.main.require('./services/SubscriberService');
 
 module.exports = (app) => {
-
     app.get('/api/subscribers/all', async (req, res) => {
         try {
             const result = await getSubscribers();
@@ -22,5 +18,4 @@ module.exports = (app) => {
             return res.status(ex.code || 500).send(ex.error);
         }
     });
-
 };
